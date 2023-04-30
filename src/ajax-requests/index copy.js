142283c641905d17f4a0a5ceb77895e2f9c34bb0
scottistas1406 <120509564+scottistas1406@users.noreply.github.com>
2023-04-30 16:52:16 -0500

    last commit

---
+++ src/ajax-requests/index copy.js
@@ -0,0 +1,122 @@
+const COHORT_NAME = '2301-ftb-et-web-pt';
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
+   export const registerUser = async (user) => {
+        
+        try {
+          const response = await fetch(
+            `${BASE_URL}/users/register`, {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              user
+            })
+          });
+          const result = await response.json();
+   
+          return result
+          console.log('*****************index.js',result)
+        } catch (err) {
+          console.error(err);
+        }
+      } ;
+ //LOGIN ***************
+ export const loginUser = async (user) => {
+  
+    try {
+      const response = await fetch(`${BASE_URL}/users/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user
+        }),
+      });
+      const result = await response.json();
+      // console.log(result);
+      return result;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  
+    
+          export const myData = async (token) => {
+            try {
+              const response = await fetch(`${BASE_URL}/users/me`, {
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization: `Bearer ${token}`,
+                },
+              });
+              const result = await response.json();
+              // console.log(result);
+              return result;
+            } catch (err) {
+              console.error(err);
+            }
+          };
+    
+
+//Posts **************
+    export  const fetchPosts = async (token) => {
+        try {
+          const response = await fetch(`${BASE_URL}/posts`,{
+            headers:{
+                'Content-type':'application/json',                
+                Authorization:'Bearer ${token}'
+            },
+          });
+      
+          const result = await response.json();
+        //  console.log(result);
+          return result
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
+     export const makePost = async (post,token) => {
+console.log(post,token)
+       
+       
+        try {
+          const response = await fetch(`${BASE_URL}/posts`, {
+            method: "POST",
+           
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+            },
+            body: JSON.stringify({
+              post
+              }),
+            
+          });
+          const result = await response.json();
+         // console.log(result);
+          return result
+        } catch (err) {
+          console.error('this is a problem',err);
+        }
+      }
+     
+    //***************Delete * just a sample delete is not working/
+    const deletePost = async () => {
+        try {
+          const response = await fetch(`${BASE_URL}/posts/5e8d1bd48829fb0017d2233b`, {
+            method: "DELETE",
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${TOKEN_STRING_HERE}`
+            }
+          });
+          const result = await response.json();
+          console.log(result);
+          return result
+        } catch (err) {
+          console.error(err);
+        }
+      }
--- src/ajax-requests/index.js
+++ src/ajax-requests/index.js
@@ -1,122 +1,127 @@
-const COHORT_NAME = '2301-ftb-et-web-pt';
-const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`
-   export const registerUser = async (user) => {
-        
-        try {
-          const response = await fetch(
-            `${BASE_URL}/users/register`, {
-            method: "POST",
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              user
-            })
-          });
-          const result = await response.json();
-   
-          return result
-          console.log('*****************index.js',result)
-        } catch (err) {
-          console.error(err);
-        }
-      } ;
- //LOGIN ***************
- export const loginUser = async (user) => {
-  
-    try {
-      const response = await fetch(`${BASE_URL}/users/login`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          user
-        }),
-      });
-      const result = await response.json();
-      // console.log(result);
-      return result;
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  
-    
-          export const myData = async (token) => {
-            try {
-              const response = await fetch(`${BASE_URL}/users/me`, {
-                headers: {
-                  "Content-Type": "application/json",
-                  Authorization: `Bearer ${token}`,
-                },
-              });
-              const result = await response.json();
-              // console.log(result);
-              return result;
-            } catch (err) {
-              console.error(err);
-            }
-          };
-    
+const COHORT_NAME = '2301-ftb-et-web-pt'
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
-//Posts **************
-    export  const fetchPosts = async (token) => {
-        try {
-          const response = await fetch(`${BASE_URL}/posts`,{
-            headers:{
-                'Content-type':'application/json',                
-                Authorization:'Bearer ${token}'
-            },
-          });
-      
-          const result = await response.json();
-        //  console.log(result);
-          return result
-        } catch (err) {
-          console.error(err);
-        }
-      };
 
-     export const makePost = async (post,token) => {
-console.log(post,token)
-       
-       
-        try {
-          const response = await fetch(`${BASE_URL}/posts`, {
-            method: "POST",
-           
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({
-              post
-              }),
-            
-          });
-          const result = await response.json();
-         // console.log(result);
-          return result
-        } catch (err) {
-          console.error('this is a problem',err);
-        }
+// USER API REQUESTS =========================================================
+export const registerUser = async (user) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/register`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user
+      }),
+    });
+    const result = await response.json();
+    // You can log ▲▲▲ the result
+    // here ▼▼▼ to view the json object before returning it
+    // console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const loginUser = async (user) => {
+  
+  try {
+    const response = await fetch(`${BASE_URL}/users/login`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user
+      }),
+    });
+    const result = await response.json();
+    // console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+
+export const myData = async (token) => {
+  try {
+    const response = await fetch(`${BASE_URL}/users/me`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    // console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+
+export const fetchPosts = async (token) => {
+  try {
+    const response = await fetch(`${BASE_URL}/posts`, {
+      headers: {
+        'Content-Type': "application/json",
+        Authorization: `Bearer ${token}`
       }
-     
-    //***************Delete * just a sample delete is not working/
-    const deletePost = async () => {
-        try {
-          const response = await fetch(`${BASE_URL}/posts/5e8d1bd48829fb0017d2233b`, {
-            method: "DELETE",
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${TOKEN_STRING_HERE}`
-            }
-          });
-          const result = await response.json();
-          console.log(result);
-          return result
-        } catch (err) {
-          console.error(err);
-        }
-      }
+    });
+
+    const result = await response.json();
+    // console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+
+export const makePost = async (post, token) => {
+  
+  try {
+    const response = await fetch(`${BASE_URL}/posts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        post
+      }),
+    });
+    const result = await response.json();
+    // console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+
+export const updatePost = async (postId, token, updatedPost) => {
+
+  try {
+    // You will need to insert a variable into the fetch template literal
+    // in order to make the POST_ID dynamic.
+    // 5e8d1bd48829fb0017d2233b is just for demonstration.
+    const response = await fetch(`${BASE_URL}/posts/${postId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        post: updatedPost,
+      }),
+    });
+    const result = await response.json();
+    console.log(result);
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+};
